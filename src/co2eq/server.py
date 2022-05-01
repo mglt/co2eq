78d2cbb4d11fc2449cdd657a8b253859d8e0de5f
pssingh21 <peeyushmansingh@gmail.com>
2022-05-01 12:34:40 +0200

    Added threading to wacth file changes and send close signal to socket on server termination

--- src/co2eq/server.py
+++ src/co2eq/server.py
@@ -7,6 +7,8 @@ from conf import CONF
 from plot_meeting import plot_meeting
 import json
 import shutil
+from threading import Thread
+import signal
 
 output_file_path = CONF['OUTPUT_DIR']
 
@@ -14,30 +16,51 @@ async def handler(websocket, path):
 
     data = await websocket.recv()
 
-    print(data)
-
     data_dict = json.loads(data)
 
     print(data_dict)
 
+
+    async def fileWatch():
+        while True:
+            if os.path.isdir(os.path.join(output_file_path, data_dict['name'])):
+                for file_name in os.listdir(os.path.join(output_file_path, data_dict['name'])):
+                    if file_name.endswith('.svg'):
+                        graph_path = os.path.join(output_file_path, data_dict['name'], file_name)
+                        with open(graph_path, "rb") as image_file:
+                            encoded_string = base64.b64encode(image_file.read())
+                        await websocket.send(encoded_string.decode('utf8'))
+                    else:
+                        continue
+            await asyncio.sleep(10)
+
+    def runFileWatch():
+        asyncio.run(fileWatch())
+    
+    _thread = Thread(target=runFileWatch)
+    _thread.start()
+
+    asyncio.Task(fileWatch())
+
+
+
     plot_meeting(data_dict)
 
-    for file_name in os.listdir(os.path.join(output_file_path, data_dict['name'])):
-        if file_name.endswith('.svg'):
-            graph_path = os.path.join(output_file_path, data_dict['name'], file_name)
-            with open(graph_path, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-            await websocket.send(encoded_string.decode('utf8'))
-        else:
-            continue
+    await asyncio.sleep(10)
 
     try:
         shutil.rmtree(os.path.join(output_file_path, data_dict['name']))
     except OSError as e:
         print("Error: %s - %s." % (e.filename, e.strerror))
 
-start_server = websockets.serve(handler, "", os.environ.get('PORT', 8000))
+async def start_server(stop):
+    async with websockets.serve(handler, "", os.environ.get('PORT', 8000)):
+        await stop
 
-asyncio.get_event_loop().run_until_complete(start_server)
+loop = asyncio.get_event_loop()
+stop = loop.create_future()
+loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
-asyncio.get_event_loop().run_forever()
+loop.run_until_complete(start_server(stop))
+
+loop.run_forever()
